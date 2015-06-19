977dcf600eed7dd88811810b06e41eba57a7b60a
Heark <hearkofsorcerey@gmail.com>
2015-06-19 19:20:24 -0400

    Update names.js

--- names.js
+++ names.js
@@ -2,3 +2,5 @@ var PART_1 = ["Hy", "Ry", "Ty", "Sy", "Ly", "He", "Re", "Te", "Me", "Ku", "Mu", 
 
 
 var PART_2 = ["gon", "ron", "son", "bun", "gun", "lun", "ay", "eng", "zu", "uko", "sem", "sen", "ion", "ireon", "izon" ]
+
+var CLASSES = ["the quick", "of Bertha", "the weak", "the poor", "of Asgard", "of the sun", "of the moon"]
