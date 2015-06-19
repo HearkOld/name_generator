d026591d0bab9fcbc482e3d1e906d062e28170dd
Heark <hearkofsorcerey@gmail.com>
2015-06-19 19:15:07 -0400

    Update names.js

--- names.js
+++ names.js
@@ -1,4 +1,4 @@
-var PART_1 = []
+var PART_1 = ["Hy", "Ry", "Ty", "Sy", "Ly", "He", "Re", "Te", "Me", "Ku", "Mu", "Hu", "Ru", "Ma", "A", "La", "Ha", "Ta", "Tr", "Pr", "Cr", "Ar"]
 
 
-var PART_2 = []
+var PART_2 = ["gon", "ron", "son", "bun", "gun", "lun", "ay", "eng", "zu", "uko", "sem", "sen", "ion", "ireon", "izon" ]
