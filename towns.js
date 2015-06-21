40302bae69a78613292341706e636234d12bbfa3
Heark <hearkofsorcerey@gmail.com>
2015-06-20 20:28:46 -0400

    Update towns.js

--- towns.js
+++ towns.js
@@ -1,4 +1,4 @@
 var towns = []
 
-var Town_Pre = ["Abr", "Aryn", "Lean", "Mynt", "Shar", "Qee", "Beso", "Omyn"]
+var Town_Pre = ["Abr", "Aryn", "Lean", "Mynt", "Shar", "Qee", "Beso", "Omyn", "Lympho", "Des", "Lyn"]
 var Town_Suf = ["nthia", "ia", "olyn", "ownia", "ian", "feea", "pora", "chi"]
