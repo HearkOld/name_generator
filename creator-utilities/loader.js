d58efac1cb8b90a3ab48dd37520495656de02737
Heark <hearkofsorcerey@gmail.com>
2015-07-29 22:52:30 -0400

    Create loader.js

---
+++ creator-utilities/loader.js
@@ -0,0 +1,9 @@
+function load(filename, filetype){
+ if (filetype=="js"){ 
+  var fileref=document.createElement('script')
+  fileref.setAttribute("type","text/javascript")
+  fileref.setAttribute("src", filename)
+ }
+ if (typeof fileref!="undefined")
+  document.getElementsByTagName("head")[0].appendChild(fileref)
+}
