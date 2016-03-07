9866752e87ecfa43f1791f0403f83e029730e1bf
Antwaun Tune Jr <antwauntune25@wcpsstudent.org>
2016-03-07 09:30:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var PART_1 = ["Hy", "Ry", "Ty", "Sy", "Ly", "He", "Re", "Te", "Me", "Ku", "Mu", "Hu", "Ru", "Ma", "A", "La", "Ha", "Ta", "Tr", "Pr", "Cr", "Ar"]
-var PART_2 = ["gon", "ron", "son", "bun", "gun", "lun", "ay", "eng", "zu", "uko", "sem", "sen", "ion", "ireon", "izon" ]
-var CLASSES = ["the quick", "of Bertha", "the weak", "the poor", "of Asgard", "of the sun", "of the moon", "the paladin", "the mage"]
+var PART_1 = ["Hy", "Ry", "Ty", "Sy", "Ly", "He", "Re", "Te", "Me", "Ku", "Mu", "Hu", "Ru", "Ma", "A", "La", "Ha", "Ta", "Tr", "Pr", "Cr", "Ar", "Le", "Kym"]
+var PART_2 = ["gon", "ron", "son", "bun", "gun", "lun", "ay", "eng", "zu", "uko", "sem", "sen", "ion", "ireon", "izon", "orio", "done"]
+var CLASSES = ["the quick", "of Bertha", "the weak", "the poor", "of Asgard", "of the sun", "of the moon", "the paladin", "the mage", "the doctor", "the king", "the beast", "the savage"]
 var Name = function(prefix, suffix, ending){
   this.prefix = PART_1[Math.floor(Math.random()*PART_1.length)];
   this.suffix = PART_2[Math.floor(Math.random()*PART_2.length)];
@@ -16,8 +16,8 @@ var write = function(){
 var generate = function(){
  document.getElementById('log').innerHTML = '<h1>'+PART_1[Math.floor(Math.random()*PART_1.length)]+ "" +PART_2[Math.floor(Math.random()*PART_2.length)]+" "+CLASSES[Math.floor(Math.random()*CLASSES.length)]+'</h1>'
 }
-var Town_Pre = ["Abr", "Aryn", "Lean", "Mynt", "Shar", "Qee", "Beso", "Omyn", "Lympho", "Des", "Lyn"]
-var Town_Suf = ["nthia", "ia", "olyn", "ownia", "ian", "feea", "pora", "chi"]
+var Town_Pre = ["Abr", "Aryn", "Lean", "Mynt", "Shar", "Qee", "Beso", "Omyn", "Lympho", "Des", "Lyn", "Von", "Yoo", "Win"]
+var Town_Suf = ["nthia", "ia", "olyn", "ownia", "ian", "feea", "pora", "chi", "ville", "inna", "chi"]
 var generateTown = function(){
  document.getElementById('log').innerHTML = '<h1>'+Town_Pre[Math.floor(Math.random()*Town_Pre.length)]+ "" +Town_Suf[Math.floor(Math.random()*Town_Suf.length)]+'</h1>'
 }
